f30e5e4025aa4f170f2e5ec2849eea967d7a1173
FrankerPool <the.lalo.frank@gmail.com>
2021-05-04 12:19:12 -0500

    test

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour{
     public Transform playerPoint;
     //
     public PlayerModel player;
-    //
+    //as
     public AudioSource backGroundMusic;
     void Awake(){
       instanciaCompartidaGameManager = this;
